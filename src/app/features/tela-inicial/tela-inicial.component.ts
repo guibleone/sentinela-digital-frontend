8553823ef9d7e3ff1fa5fe51ad2c3f44a19d45f5
Guilherme Bastos Leone <guilhermebastosleone@gmail.com>
2026-02-28 15:25:40 -0300

    feat: add hero component.
    - Implemented hero component with a main section for internet safety education.
    - Added responsive styles for layout and typography in SCSS.

--- src/app/features/tela-inicial/tela-inicial.component.ts
+++ src/app/features/tela-inicial/tela-inicial.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from "./hero/hero.component";
 
 @Component({
   selector: 'app-tela-inicial',
-  imports: [],
+  imports: [HeroComponent],
   templateUrl: './tela-inicial.component.html',
   styleUrl: './tela-inicial.component.scss'
 })
