7d021f46be5c143679224dc7eeafa875aff67677
Guilherme Bastos Leone <guilhermebastosleone@gmail.com>
2026-02-28 15:09:56 -0300

    (feat): refatora estrutura do projeto, renomeia arquivos e adiciona componentes de navegação

---
+++ src/app/features/tela-inicial/tela-inicial.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-inicial',
+  imports: [],
+  templateUrl: './tela-inicial.component.html',
+  styleUrl: './tela-inicial.component.scss'
+})
+export class TelaInicialComponent {
+
+}
